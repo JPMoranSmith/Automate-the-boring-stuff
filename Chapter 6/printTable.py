f35501d919d079ce6891d667459d485b54399007
JPMoranSmith <33219219+JPMoranSmith@users.noreply.github.com>
2019-01-15 15:06:39 +0000

    Update printTable.py
    Updated to add try except to allow variable list lengths in list collections passed to function

--- Chapter 6/printTable.py
+++ Chapter 6/printTable.py
@@ -2,17 +2,23 @@
 # printTable.py - takes a list of data and prints into a table
 
 tableData = [['apples', 'oranges', 'cherries', 'banana'],
-             ['Alice', 'Bob', 'Carol', 'David'],
-             ['dogs', 'cats', 'moose', 'goose']]
+             ['Alice', 'Bob', 'Carol', 'David', 'Jerry', 'Brian'],
+             ['dogs', 'cats', 'moose', 'goose', 'cricket']]
 
 def printTable(data):
+        maxLen = len(max(data, key=len))
         colWidth = [0] * len(data)
-        for i in range(4):
+        for i in range(maxLen):
                 for n in range(len(data)):
                         if colWidth[n] == 0:
                                 colWidth[n] = len(max(data[n], key=len))
-                        print (data[n][i].rjust(colWidth[n]) + ' |'),
+                        try:
+                                print (data[n][i].rjust(colWidth[n]) + ' |', end='')
+                        except IndexError:
+                                print ('NULL'.rjust(colWidth[n]) + ' |', end='')
                 print()
         
 printTable(tableData)
+
+
 
